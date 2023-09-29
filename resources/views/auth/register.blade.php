12015601770e271bee04e9fcd7feb3ac25507341
waledzee <waledzaki201@gmail.com>
2023-09-29 20:01:48 +0300

    regiteration page finished

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -73,5 +73,70 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
+
+
+<div class="reservation-form">
+    <div class="container">
+      <div class="row">
+        
+        <div class="col-lg-12">
+          <form id="reservation-form"  method="post" role="search" action="{{ route('register') }}">
+            @csrf
+            <div class="row">
+              <div class="col-lg-12">
+                <h4 style="color: black;">Register</h4>
+              </div>
+              <div class="col-md-12">
+                <fieldset>
+                    <label for="Name" class="form-label">Username</label>
+                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+@error('name')
+    <span class="invalid-feedback" role="alert">
+        <strong>{{ $message }}</strong>
+    </span>
+@enderror                </fieldset>
+              </div>
+
+              <div class="col-md-12">
+                  <fieldset>
+                      <label for="Name" class="form-label">Your Email</label>
+                      <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+@error('email')
+    <span class="invalid-feedback" role="alert">
+        <strong>{{ $message }}</strong>
+    </span>
+@enderror                  </fieldset>
+              </div>
+           
+              <div class="col-md-12">
+                <fieldset>
+                    <label for="Name" class="form-label">Your Password</label>
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+@error('password')
+    <span class="invalid-feedback" role="alert">
+        <strong>{{ $message }}</strong>
+    </span>
+@enderror                </fieldset>
+              </div>
+              <div class="col-md-12">
+                <fieldset>
+                    <label for="Name" class="form-label">Confirm Password</label>
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                </fieldset>
+              </div>
+              <div class="col-lg-12">                        
+                  <fieldset>
+                      <button type="submit" class="main-button">register</button>
+                  </fieldset>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
