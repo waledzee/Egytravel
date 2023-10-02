d6ca5395e277a5b4fb98b14964ca3be4cc44c5bf
waledzee <waledzaki201@gmail.com>
2023-10-02 11:08:25 +0300

    displaying countires from DB

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\models\country\country;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $countries=country::select()->orderBy("id","desc")->get();
+        return view('home',compact('countries'));
     }
 }
