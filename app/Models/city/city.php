ee6ddc132efc5528cb74748f26ca970f766398a3
waledzee <waledzaki201@gmail.com>
2023-10-06 06:51:02 +0300

    city model created

---
+++ app/Models/city/city.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\city;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class city extends Model
+{
+    use HasFactory;
+}
