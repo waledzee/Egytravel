af8497bed4bf59619dce3df1575576c28b91f8a0
waledzee <waledzaki201@gmail.com>
2023-10-06 06:56:33 +0300

    cities migration created succeffuly

--- app/Models/city/city.php
+++ app/Models/city/city.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class city extends Model
 {
     use HasFactory;
+    protected $table ="cities"; 
+    protected $fillable =[
+        "name",
+        "image",
+        "prive",
+        "num_days",
+        "country_id",
+        
+    ];
 }
